e43fa2335e396f0f6cf7e0cded0e01b1c92856ae
kateniedergeses <60635220+kateniedergeses@users.noreply.github.com>
2023-05-18 16:02:31 -0500

    First Commit JavaInt

---
+++ JavaInt.java
@@ -0,0 +1,11 @@
+package JavaInt;
+
+/**
+ * JavaInt
+ */
+public class JavaInt {
+    public static void main(String[] args) {
+        System.out.println("Hello Tyler Interview!");
+    }
+    
+}
